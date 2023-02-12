60610f63b7f464b4aa0fb7864c5037b3112dd24f
MillenaSousa <112016777+MillenaSousa@users.noreply.github.com>
2023-02-12 15:36:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var user = document.getElementById('user')
 var computer = document.getElementById('comput')
-var result = document.getElementById('result')
+var audio = document.getElementById('mp3-figth')
 var resullt = document.getElementById('resullt')
 var figth = document.getElementById('img-figth')
 
@@ -12,114 +12,116 @@ select.addEventListener('click', function (e) {
     var comput = Math.floor(Math.random() * 3);
 
     // 0 pedra -- 1 papel -- 2 tesoura 
+    inicio()
 
 
-    if (comput == 0) {
-        var com = 'pedra'
-    } else if (comput == 1) {
-        var com = 'papel'
-    } else {
-        var com = 'tesoura'
+    function inicio() {
+        audio.setAttribute('src', 'Efeito_sonoro_Mortal_Kombat_Fight_-_Mortal_Kombat_sound_effect_FightMP3_128K.mp3')
+
     }
+    setTimeout(() => { game() }, 1000);
 
-    console.log(com)
-    if (valorId === 'img-rock' && comput === 0) {
-        // empate
-        resullt.innerHTML += `<h3> EMPATE </h3>`
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/pedra.png');
-        computer.setAttribute('src', 'img/pedra.png');
-        recarregar()
 
-    } else if (valorId === 'img-rock' && comput === 1) {
-        // papel ganha
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/pedra.png');
-        computer.setAttribute('src', 'img/papel.png');
+    function game() {
 
-        resullt.innerHTML += `<h3> GANHADOR </h3>`
-        resullt.innerHTML += `<img src="img/papel.png" >`
+      
+        if (valorId === 'img-rock' && comput === 0) {
+            // empate
+            resullt.innerHTML += `<h3> EMPATE </h3>`
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/pedra.png');
+            computer.setAttribute('src', 'img/pedra.png');
+            recarregar()
 
-        recarregar()
-    } else if (valorId === 'img-rock' && comput === 2) {
-        // pedra ganha
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/pedra.png');
-        computer.setAttribute('src', 'img/tesouraa.png');
+        } else if (valorId === 'img-rock' && comput === 1) {
+            // papel ganha
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/pedra.png');
+            computer.setAttribute('src', 'img/papel.png');
 
-        resullt.innerHTML += `<h3> GANHADOR </h3>`
-        resullt.innerHTML += `<img src="img/pedra.png" >`
+            resullt.innerHTML += `<h3> GANHADOR </h3>`
+            resullt.innerHTML += `<img src="img/papel.png" >`
 
-        recarregar()
+            recarregar()
+        } else if (valorId === 'img-rock' && comput === 2) {
+            // pedra ganha
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/pedra.png');
+            computer.setAttribute('src', 'img/tesouraa.png');
+
+            resullt.innerHTML += `<h3> GANHADOR </h3>`
+            resullt.innerHTML += `<img src="img/pedra.png" >`
+
+            recarregar()
+        }
+
+
+        if (valorId === 'img-paper' && comput === 1) {
+            // empate
+            resullt.innerHTML += `<h3> EMPATE </h3>`
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/papel.png');
+            computer.setAttribute('src', 'img/papel.png');
+
+            recarregar()
+        } else if (valorId === 'img-paper' && comput === 0) {
+            // papel ganha
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/papel.png');
+            computer.setAttribute('src', 'img/pedra.png');
+
+            resullt.innerHTML += `<h3> GANHADOR </h3>`
+            resullt.innerHTML += `<img src="img/papel.png" >`
+
+            recarregar()
+        } else if (valorId === 'img-paper' && comput === 2) {
+            // tesoura ganha
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/papel.png');
+            computer.setAttribute('src', 'img/tesouraa.png');
+
+            resullt.innerHTML += `<h3> GANHADOR </h3>`
+            resullt.innerHTML += `<img src="img/tesouraa.png" >`
+
+            recarregar()
+        }
+
+        else if (valorId === 'img-scissors' && comput === 2) {
+            // empate
+            resullt.innerHTML += `<h3> EMPATE </h3>`
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/tesouraa.png');
+            computer.setAttribute('src', 'img/tesouraa.png');
+
+            recarregar()
+        } else if (valorId === 'img-scissors' && comput === 0) {
+            //    pedra vence
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/tesouraa.png');
+            computer.setAttribute('src', 'img/pedra.png');
+
+            resullt.innerHTML += `<h3> GANHADOR </h3>`
+            resullt.innerHTML += `<img src="img/pedra.png" >`
+
+            recarregar()
+        } else if (valorId === 'img-scissors' && comput === 1) {
+            // tesoura vence
+            figth.setAttribute('src', 'img/fight.png');
+            user.setAttribute('src', 'img/tesouraa.png');
+            computer.setAttribute('src', 'img/papel.png');
+
+            resullt.innerHTML += `<h3> GANHADOR </h3>`
+            resullt.innerHTML += `<img src="img/tesouraa.png" >`
+
+            recarregar()
+        }
+        return
     }
-
-
-    if (valorId === 'img-paper' && comput === 1) {
-        // empate
-        resullt.innerHTML += `<h3> EMPATE </h3>`
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/papel.png');
-        computer.setAttribute('src', 'img/papel.png');
-
-        recarregar()
-    } else if (valorId === 'img-paper' && comput === 0) {
-        // papel ganha
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/papel.png');
-        computer.setAttribute('src', 'img/pedra.png');
-
-        resullt.innerHTML += `<h3> GANHADOR </h3>`
-        resullt.innerHTML += `<img src="img/papel.png" >`
-
-        recarregar()
-    } else if (valorId === 'img-paper' && comput === 2) {
-        // tesoura ganha
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/papel.png');
-        computer.setAttribute('src', 'img/tesouraa.png');
-
-        resullt.innerHTML += `<h3> GANHADOR </h3>`
-        resullt.innerHTML += `<img src="img/tesouraa.png" >`
-
-        recarregar()
-    }
-
-    else if (valorId === 'img-scissors' && comput === 2) {
-        // empate
-        resullt.innerHTML += `<h3> EMPATE </h3>`
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/tesouraa.png');
-        computer.setAttribute('src', 'img/tesouraa.png');
-
-        recarregar()
-    } else if (valorId === 'img-scissors' && comput === 0) {
-        //    pedra vence
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/tesouraa.png');
-        computer.setAttribute('src', 'img/pedra.png');
-
-        resullt.innerHTML += `<h3> GANHADOR </h3>`
-        resullt.innerHTML += `<img src="img/pedra.png" >`
-
-        recarregar()
-    } else if (valorId === 'img-scissors' && comput === 1) {
-        // tesoura vence
-        figth.setAttribute('src', 'img/fight.png');
-        user.setAttribute('src', 'img/tesouraa.png');
-        computer.setAttribute('src', 'img/papel.png');
-
-        resullt.innerHTML += `<h3> GANHADOR </h3>`
-        resullt.innerHTML += `<img src="img/tesouraa.png" >`
-
-        recarregar()
-    }
-
-
 
 })
 
 function recarregar() {
-    setTimeout(() => { window.location.reload(true) }, 2000);
+    setTimeout(() => { window.location.reload(true) }, 3000);
 
 
     return
